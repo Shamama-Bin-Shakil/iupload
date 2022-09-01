7f4025b418c41b1fc05e70f3d0efa72625d42494
shamama <rohishamama@gmail.com>
2022-09-01 00:19:24 -0700

    version 0.1

---
+++ main.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+// const morgan = require('morgan');
+const cors = require('cors');
+const session = require('express-session');
+require('dotenv').config();
+const connectDB = require('./server/db/db');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// DB_URL=mongodb+srv://Shamama:KKZfXA2CTd9fTgAG@cluster0.cb9sllr.mongodb.net/images
+
+// Connection mongoDB
+connectDB();
+
+app.use(session({
+    secret: process.env.SESSION_KEY,
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// use logger create
+// app.use(morgan("tiny"));
+
+// Use Cors
+app.use(cors())
+
+
+// Use middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Use engine
+app.set("view engine", "ejs");
+
+// Load Public Folder
+app.use(express.static(path.join(__dirname, "./public")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+// Change Views Folder convert to templates
+app.set("views", path.join(__dirname, "./templates"));
+
+// Use Router
+app.use("/", require("./server/router/router"));
+
+// SERVER IS LISTEN
+app.listen(PORT, () => console.log(`SERVER IS LISTEN http://localhost:${PORT}`))
---
+++ server/model/model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    email: {
+        type: String,
+        require: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    },
+    image: {
+        type: String,
+        require: true,
+    },
+    apikey: {
+        type: String,
+        require: true,
+    },
+});
+
+const User = mongoose.model("user", schema);
+
+module.exports = User;
