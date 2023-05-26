018f14ff30584546e2d4841ccfbb26bc0a652a9f
shamamaali <rohishamama@gmail.com>
2023-05-26 15:31:00 +0500

    iupload vercel configuration

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 // const morgan = require('morgan');
-const cors = require('cors');
-const session = require('express-session');
-require('dotenv').config();
-const connectDB = require('./server/db/db');
+const cors = require("cors");
+const session = require("express-session");
+require("dotenv").config();
+const connectDB = require("./server/db/db");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 // Connection mongoDB
 connectDB();
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_KEY,
     resave: false,
     saveUninitialized: false,
-}))
+  })
+);
 
 // use logger create
 // app.use(morgan("tiny"));
 
 // Use Cors
-app.use(cors())
-
+app.use(cors());
 
 // Use middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Use engine
 app.set("view engine", "ejs");
@@ -42,4 +43,6 @@ app.set("views", path.join(__dirname, "./templates"));
 app.use("/", require("./server/router/router"));
 
 // SERVER IS LISTEN
-app.listen(PORT, () => console.log(`SERVER IS LISTEN http://localhost:${PORT}`))
+app.listen(PORT, () =>
+  console.log(`SERVER IS LISTEN http://localhost:${PORT}`)
+);
--- server/router/router.js
+++ server/router/router.js
@@ -5,72 +5,74 @@ const controller = require("../controller/controller");
 const userdata = require("../middleware/userdata");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, callback) {
-      callback(null, "uploads/");
-    },
-    filename: function (req, file, callback) {
-      callback(null, Date.now() + "-" + file.originalname);
-    },
-  });
+  destination: function (req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
 const upload = multer({
-    storage: storage,
-  });
+  storage: storage,
+});
 
 // Home Router
 route.get("/", (req, res) => {
-    res.render("index", { user: req.session.email});
+  res.render("index", { user: req.session.email });
 });
 
 // Login Router
 route.get("/login", (req, res) => {
-    if(req.session.email) {
-        res.render("index", { user: req.session.email});
-    }else {
-        res.render("login", { user: req.session.email});
-    }
-})
+  if (req.session.email) {
+    res.render("index", { user: req.session.email });
+  } else {
+    res.render("login", { user: req.session.email });
+  }
+});
 
 // Upload Router
 route.get("/upload", (req, res) => {
-  if(req.session.email) {
-    res.render("upload", { user: req.session.email});
-  }else {
-    res.render("index", { user: req.session.email});
+  if (req.session.email) {
+    res.render("upload", { user: req.session.email });
+  } else {
+    res.render("index", { user: req.session.email });
   }
 });
 
 // Profile Router
 route.get("/profile", (req, res) => {
-  if(req.session.email) {
-    res.render("profile", { user: req.session.email});
-  }else {
-    res.render("index", { user: req.session.email});
+  if (req.session.email) {
+    res.render("profile", { user: req.session.email });
+  } else {
+    res.render("index", { user: req.session.email });
   }
 });
 
 // User Image
 route.get("/userimage", (req, res) => {
-  if(req.session.email) {
-    res.render("userimage", { user: req.session.email});
-  }else {
-    res.render("index", { user: req.session.email});
+  if (req.session.email) {
+    res.render("userimage", { user: req.session.email });
+  } else {
+    res.render("index", { user: req.session.email });
   }
 });
 
 // API Key
 route.get("/apikey", (req, res) => {
-  if(req.session.email) {
-    res.render("apikey", { user: req.session.email, API_KEY: req.session.apikey});
-  }else {
-    res.render("index", { user: req.session.email});
+  if (req.session.email) {
+    res.render("apikey", {
+      user: req.session.email,
+      API_KEY: req.session.apikey,
+    });
+  } else {
+    res.render("index", { user: req.session.email });
   }
 });
 
 // Router API
 route.get("/api", (req, res) => {
-    res.render("api", { user: req.session.email, API_KEY: req.session.apikey});
+  res.render("api", { user: req.session.email, API_KEY: req.session.apikey });
 });
-
 
 // API's
 route.post("/api/register", controller.register);
@@ -84,7 +86,12 @@ route.post("/api/userimagedelete", userdata, controller.userimagedelete);
 
 route.post("/api/uploadimage", userdata, upload.any(), controller.uploadimage);
 
-route.post("/api/profileimageupload", userdata, upload.any(), controller.profileimageupload);
+route.post(
+  "/api/profileimageupload",
+  userdata,
+  upload.any(),
+  controller.profileimageupload
+);
 
 route.post("/api/fetchimage", controller.fetchimage);
 
